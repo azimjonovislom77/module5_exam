e94b3c55d1f06b5124a336da18626ce30453db5b
ISLOM <88037333+azimjonovislom77@users.noreply.github.com>
2025-02-15 13:45:53 +0500

    db.py

--- db.py
+++ db.py
@@ -33,7 +33,7 @@ def create_table():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS todos (
             id SERIAL PRIMARY KEY,
-            user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
             title VARCHAR(255) NOT NULL,
             description TEXT NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
